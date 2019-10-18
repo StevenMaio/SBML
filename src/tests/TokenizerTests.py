d93da2080fcc718d3fa38f41dde2ef55e35b55cd
Steven Maio <stevenmaio.321@gmail.com>
2019-10-18 17:53:28 -0400

    Added some unit tests for Tokenizer
    Also renamed Parser to Tokenizer

---
+++ src/tests/TokenizerTests.py
@@ -0,0 +1,38 @@
+import unittest
+
+from functools import reduce
+
+# add src to the python path
+import sys
+from pathlib import Path
+root = Path(__file__).resolve().parents[2]
+sys.path.append(str(root))
+
+from src.parser.SBMLTokenizer import SBMLTokenizer
+
+class TokenizerTests(unittest.TestCase):
+
+    tokenizer = SBMLTokenizer()
+
+    def test_parse_reals(self):
+        test_input = [
+                "-3.14",
+                "3.14",
+                "0.3e1",
+                "-17.02",
+        ]
+        expected = [
+            (-3.14,  'REAL'),
+            (3.14,   'REAL'),
+            (3.0,   'REAL'),
+            (-17.02, 'REAL'),
+        ]
+        results = reduce(lambda x,y : x+y,
+                         map(lambda x: self.tokenizer.test(x),
+                             test_input))
+        # extract only the value and type
+        results = list(map(lambda x: (x.value, x.type), results))
+        self.assertEqual(results, expected)
+
+if __name__ == '__main__':
+    unittest.main()
